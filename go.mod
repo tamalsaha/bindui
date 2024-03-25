9326921c30c7d8c8550ceea18780b9f3b339f743
Tamal Saha <tamal@appscode.com>
2024-03-24 18:24:53 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/tamalsaha/bindui
+
+go 1.22.1
+
+require k8s.io/apimachinery v0.29.3
+
+require github.com/gogo/protobuf v1.3.2 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"embed"
+	"fmt"
+	"html"
+	"html/template"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"log"
+	"net/http"
+)
+
+//go:embed *.gohtml
+var fs embed.FS
+
+type CRD struct {
+	GVK    schema.GroupVersionKind
+	Scoped string
+	Bound  bool
+	Icon   string
+}
+
+type CRDsInfo struct {
+	SessionID, ClusterName string
+	CRDs                   []CRD
+}
+
 func main() {
-	
+	http.HandleFunc("GET /bind", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFS(fs, "*.gohtml")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.ExecuteTemplate(w, "resources.gohtml", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	http.HandleFunc("POST /bind", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
